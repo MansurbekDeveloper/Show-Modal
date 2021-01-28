d29e8c1d07c8a3664aa1239ca1d1dafd58a59663
MansurbekDeveloper <cool.mansurbek@bk.ru>
2021-01-28 09:14:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', closeModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
